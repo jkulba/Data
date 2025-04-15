d2a769dee3594e70a81dd4d2a3e6c2bf0f7f9450
James Kulba <jkulba@gmail.com>
2025-04-14 17:42:37 -0700

    Add sqlserver

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ A node module used to serve JSON endpoints of supplied data in a db.json file.  
 **(2) PostgreSQL**
 Docker compose files to host the PostgreSQL server and the pgAdmin tool.
 
+**(3) MS SQL Server**
+Bash script uses latest version.
+
---
+++ sqlserver/sql.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+CONTAINER_NAME="mssql-server"
+IMAGE="mcr.microsoft.com/mssql/server:2022-latest"
+SA_PASSWORD="P@ssword92"
+PORT=1433
+VOLUME_NAME="mssql-data"
+
+start_container() {
+    echo "Starting SQL Server container..."
+    
+    # Create volume if it doesn't exist
+    podman volume inspect $VOLUME_NAME > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Creating volume $VOLUME_NAME..."
+        podman volume create $VOLUME_NAME
+    fi
+
+    podman run -d \
+        --name $CONTAINER_NAME \
+        --network host \
+        -e 'ACCEPT_EULA=Y' \
+        -e "SA_PASSWORD=$SA_PASSWORD" \
+        -v $VOLUME_NAME:/var/opt/mssql \
+        $IMAGE
+}
+
+stop_container() {
+    echo "Stopping SQL Server container..."
+    podman stop $CONTAINER_NAME
+    podman rm $CONTAINER_NAME
+}
+
+status_container() {
+    podman ps -a --filter "name=$CONTAINER_NAME"
+}
+
+case "$1" in
+    start)
+        start_container
+        ;;
+    stop)
+        stop_container
+        ;;
+    status)
+        status_container
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|status}"
+        ;;
+esac
+
